e2da69c6c45b9cab8b9e2a99f1247e437f417b99
JanaZhak <93000882+JanaZhakPl@users.noreply.github.com>
2022-01-16 00:12:32 +0100

    Add files via upload

---
+++ Lesson_11/SortingDocumnetNumbers.java
@@ -0,0 +1,40 @@
+package com.teachmeskills.Lesson_11;
+
+import java.io.*;
+
+public class SortingDocumnetNumbers {
+
+    public static void main(String[] args)  {
+        String line;
+        try {
+            FileReader myFile = new FileReader("C:\\Users\\wikto\\OneDrive\\Рабочий стол\\information\\information.txt");
+            BufferedReader num = new BufferedReader(myFile);
+            BufferedWriter validNums = new BufferedWriter(new FileWriter("validOutput.txt"));
+            BufferedWriter invalidNums = new BufferedWriter(new FileWriter("invalidOutput.txt"));
+
+            while ((line = num.readLine()) != null) {
+                if (line.length() == 15 && (line.toString().startsWith("contract") || line.toString().startsWith("docnum"))) {
+                    validNums.write(line + "\n");
+                    //System.out.println(line);
+
+                } else if (line.length() != 15) {
+                    invalidNums.write(line + " - The number must be 15 characters long" + "\n");
+                    //System.out.println(line);
+
+                } else if (!(line.toString().startsWith("docnum") || line.toString().startsWith("contract"))) {
+                    invalidNums.write(line + " - The number must start with \"contract\" or \"docnum\"" + "\n");
+                    //System.out.println(line);
+
+                } else {
+                    invalidNums.write(line + "\n");
+                }
+            }
+            myFile.close();
+            validNums.close();
+            invalidNums.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
